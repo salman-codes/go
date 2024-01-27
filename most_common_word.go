80beea2630513f152a62044439e26762940b9b4a
Salman <salman@staqo.com>
2024-01-27 19:42:14 +0530

    More Practice Examples

---
+++ most_common_word.go
@@ -0,0 +1,34 @@
+// What is the most common word in Rumi's poem?
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// poem is being passed as function argument
+var poem = `
+those who do not feel this love
+pulling them like a river
+those who do not drink dawn
+like a cup of spring water
+or take in sunset like supper
+those who do not want to change
+let them sleep
+`
+
+func main() {
+	frequency := make(map[string]int)
+	for _, word := range strings.Fields(poem) {
+		frequency[word]++
+	}
+
+	maxW, maxC := "", 0
+	for w, c := range frequency {
+		if c > maxC {
+			maxW, maxC = w, c
+		}
+	}
+	fmt.Println(frequency)
+	print(maxW)
+}
